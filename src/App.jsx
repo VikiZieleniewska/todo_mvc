2c3a7fd477ff51cdb26ca2adf4fe9d8e356be9f0
Viki Zieleniewska <viktoriya.zieleniewska@gmail.com>
2023-05-29 13:00:40 +0200

    todo with add todo, show todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function getId() {
+    if (!tasks.length) return 1;
+
+    return Math.max(...tasks.map((task) => task.id)) + 1;
+  }
+
+  function handleInput(event) {
+    setValue(event.target.value);
+  }
+
+  function handleAddTask(event) {
+    if (event.key === "Enter") {
+      setTasks([
+        ...tasks,
+        {
+          id: getId(),
+          name: value,
+          status: "active",
+        },
+      ]);
+      setValue("");
+    }
+  }
+
+  function handleDelite(event) {}
+
+  return (
+    <>
+      <h1>todos</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={handleInput}
+        onKeyUp={handleAddTask}
+      />
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>{task.name}</span>
+            <button onClick={handleDelite}>delite</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
